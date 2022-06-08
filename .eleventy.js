82cb97419534303b2b2686293a36230a254eac1d
zeljko <mitke013@gmail.com>
2022-06-08 17:56:39 +0200

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,44 @@
+const htmlmin = require('html-minifier');
+
+function eleventyConfig(config) {
+	// Passthroughs
+	config.addPassthroughCopy("src/img");
+
+	// Layout aliases
+	config.addLayoutAlias("base", "layouts/base.njk");
+	config.addLayoutAlias("portal", "layouts/portal_base.njk");
+
+	// Minify HTML
+	const isProduction = process.env.ELEVENTY_ENV === "production";
+
+	let htmlMinify = function(value, outputPath) {
+		if (outputPath && outputPath.indexOf('.html') > -1) {
+			return htmlmin.minify(value, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+				minifyCSS: false
+			});
+		}
+	}
+
+	// html min only in production
+	if (isProduction) {
+		config.addTransform("htmlmin", htmlMinify);
+	}
+
+	// Configuration
+	return {
+		dir: {
+			input: "src",
+			output: "dist",
+			includes: "includes",
+			data: "data",
+		},
+		templateFormats: ["html", "njk", "md", "11ty.js"],
+		htmlTemplateEngine: "njk",
+		markdownTemplateEngine: "njk",
+	};
+};
+
+module.exports = eleventyConfig;
---
+++ README.md
@@ -0,0 +1,22 @@
+# Bootstrap Starter Kit
+
+```
+npm install
+```
+
+To run a local server and watch for changes:
+
+```
+npm start
+```
+
+To build for production:
+
+```
+npm run build
+```
+
+Open either of these:
+
+    Local: http://localhost:8080
+    External: http://192.168.0.20:8080
